cfef9cce45ffbf27f306b3519924709df6e95860
clokey3 <97642808+clokey3@users.noreply.github.com>
2022-04-24 22:50:38 -0400

    Update Choose your own Adventure.py

--- Choose your own Adventure.py
+++ Choose your own Adventure.py
@@ -12,11 +12,24 @@ if q1.lower() == "left":
         q12 = ("You see an alligator swimming directly at you. Do you fight or keep swimminng?")
             if q12.lower() == 'fight':
                 print('You manage to fend off the alligator, but not without taking some damage."
-    elif q2.lower() == "walk":
-        print("You walk for many miles, ran out of water and you lost the game")
+                Break
+            elif q12.lower() == 'swim':
+                print('You try to swim faster but the alligator is too fast for you in the water. he takes a big bite out of your leg before you reach the other side.')
+                break
+    elif q11.lower() == "walk":
+        q13 = input("A badger growls at you and looks prone to fight. Do you fight or runaway?")
+            if q13.lower() == 'fight':
+                print('You take some nasty hits from a ferocious beast, but eventually it retreats back to its lair')
+                break
+            elif q13.lower() == 'run':
+                print('Good choice! fighting an animal like that would be suicide!')
+                break
+            else:
+                print(error)
+                break
     else:
         print(error)
 elif q1.lower() == "right":
-    answer = input()
+    answer = input('You come across a large mountain. Do you go around )
 else:
     print(error)
